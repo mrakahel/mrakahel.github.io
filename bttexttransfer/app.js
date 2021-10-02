5325968a0150ad7be6ad8cbc5e7d04917a83f071
mrakahel <k.yama118@gmail.com>
2021-10-02 15:59:06 +0900

    修正

--- bttexttransfer/app.js
+++ bttexttransfer/app.js
@@ -1,7 +1,12 @@
 window.addEventListener('load', async e => {
     if("serviceWorker" in navigator){
         try{
-            navigator.serviceWorker.register('sw.js');
+            navigator.serviceWorker.register('sw.js')
+            .then(registratioin => {
+                registratioin.onupdatefound = function() {
+                    registratioin.update();
+                }
+            })
             console.log(`SW registered`);
         }catch(error){
             console.log(`SW not registered`);
