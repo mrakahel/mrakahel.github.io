a9accd9ed004bdd18dcaefaa13cf1630c1197ca8
mrakahel <k.yama118@gmail.com>
2021-10-06 18:14:03 +0900

    長文対応

---
+++ bttexttransfer2/app.js
@@ -0,0 +1,183 @@
+
+//navigator.bluetooth.addEventListener('onavailabilitychanged', OnAvailabilityChanged);
+  
+var bluetoothDevice;
+var characteristic;
+
+var TEXT_SERVICE_UUID = 'b07ff626-4b79-0001-89e5-fae40ab7e07f';
+var TEXT_CHARACTERISTIC_UUID = 'b07ff626-4b79-0004-89e5-fae40ab7e07f';
+//var TEXT_SERVICE_UUID = '0000aaa0-0000-1000-8000-aabbccddeeff'
+//var TEXT_CHARACTERISTIC_UUID = '0000aaa2-0000-1000-8000-aabbccddeeff';
+
+var status;
+
+//ボタンイベントリスナー
+d3.select("#connect").on("click", connect);
+d3.select("#disconnect").on("click", disconnect);
+d3.select("#reconnect").on("click", reconnect);
+d3.select("#send").on("click", sendMessage);
+
+
+//デバイスに接続する
+async function connect() {
+    let options = {};
+
+    //options.acceptAllDevices = true;
+    options.filters = [
+        {services: [TEXT_SERVICE_UUID]}
+    ];
+
+    updateStatus('Connecting...');
+
+    let server;
+    let service;
+    let chara;
+    try{
+        bluetoothDevice = await navigator.bluetooth.requestDevice(options)
+        console.log("device", bluetoothDevice);
+        updateDevicename(bluetoothDevice.name);
+        bluetoothDevice.ongattserverdisconnected = onGattServerDisconnected;
+        server = bluetoothDevice.gatt.connect();
+    
+        if(bluetoothDevice.gatt.connected){
+            server = bluetoothDevice.gatt;
+        }else{
+            server = await bluetoothDevice.gatt.connect();
+        }
+        console.log("server", server);
+        service = await server.getPrimaryService(TEXT_SERVICE_UUID);
+        console.log("service", service);
+        chara = await service.getCharacteristic(TEXT_CHARACTERISTIC_UUID)
+        console.log("characteristic", chara);
+        //alert("BLE接続が完了しました。");
+        updateStatus('Connected');
+        characteristic = chara;    
+    }catch(error){
+        console.log(error);
+        updateStatus(error);
+    }
+}
+
+//メッセージを送信
+async function sendMessage() {
+    const maxchunk = 200;
+    if(text === "") return;
+    //alert("bluetoothDevice:"+bluetoothDevice+" connected:"+bluetoothDevice.gatt.connected+" characteristic:"+characteristic);
+    if (!bluetoothDevice || !bluetoothDevice.gatt.connected || !characteristic) return ;
+    const text = document.querySelector("#message").value;
+    //  alert(text);
+    const arrayBuf = new TextEncoder().encode(text);
+    try{
+        let i = 0;
+        let senddata;
+        let response;
+        while(i <= arrayBuf.length){
+            if(arrayBuf > i+maxchunk){
+                senddata = [0x01].push(arrayBuf.slice(i, i+maxchunk-1));
+            }else{
+                senddata = [0x00].push(arrayBuf);
+            }
+            i += maxchunk; 
+            response = await characteristic.writeValueWithoutResponse(senddata);
+        }
+        clearText();
+    }catch(error){
+        alert('send failed');
+    }
+
+}
+
+//BLE切断処理
+function disconnect() {
+    if (!bluetoothDevice || !bluetoothDevice.gatt.connected){
+        console.log("device", bluetoothDevice);
+        return;
+    } 
+    bluetoothDevice.onGattServerDisconnected = "";
+    bluetoothDevice.gatt.disconnect();
+    bluetoothDevice = undefined;
+    updateDevicename("");
+    updateStatus("Disconnected");
+    //alert("BLE接続を切断しました。");
+}
+
+async function reconnect() {
+    if (!bluetoothDevice) {
+        console.log("device is undefined");
+        return;
+    }
+    console.log("device", bluetoothDevice);
+    let server;
+    let service;
+    let chara;
+    try{
+        if(bluetoothDevice.gatt.connected){
+            server = bluetoothDevice.gatt;
+        }else{
+            server = await bluetoothDevice.gatt.connect();
+        }
+        console.log("server", server);
+        service = await server.getPrimaryService(TEXT_SERVICE_UUID);
+        console.log("service", service);
+        chara = await service.getCharacteristic(TEXT_CHARACTERISTIC_UUID)
+        console.log("characteristic", chara);
+        updateStatus('Connected');
+        characteristic = chara; 
+        return true;   
+    }catch(error){
+        console.log(error);
+        updateStatus(error);
+    }
+    return false;
+}
+
+function updateStatus(state) {
+    let elm = document.getElementById('status');
+    elm.textContent = state;
+}
+
+function updateDevicename(name) {
+    let elm = document.getElementById('devicename');
+    elm.textContent = name;
+}
+
+function clearText() {
+    document.querySelector("#message").value = "";
+}
+
+async function onAvailabilityChanged() {
+    let availability = await navigator.bluetooth.getAvailability();
+    if(!availability) {
+        alert("Bluetooth not available");
+        updateStatus("Disconnected");
+    }
+}
+
+async function onGattServerDisconnected() {
+    const maxretry = 3;
+    if(bluetoothDevice) {
+        updateStatus("Reconnecting...")
+        for(let step = 0; step < maxretry; step++){
+            if(await reconnect()) return;
+        }
+    }
+    updateStatus("Disconnected");
+}
+
+window.addEventListener('load', async e => {
+    if("serviceWorker" in navigator){
+        try{
+            navigator.serviceWorker.register('sw.js')
+            .then(registratioin => {
+                registratioin.onupdatefound = function() {
+                    registratioin.update();
+                }
+            })
+            console.log(`SW registered`);
+        }catch(error){
+            console.log(`SW not registered`);
+        }
+    }
+    navigator.bluetooth.onavailabilitychanged = onAvailabilityChanged;
+});
+
---
+++ bttexttransfer2/sw.js
@@ -0,0 +1,102 @@
+var CACHE_NAME  = "texttransfer";
+var urlsToCache = [
+    "index.html",
+    "https://cdnjs.cloudflare.com/ajax/libs/d3/4.3.0/d3.min.js",
+    "style.css",
+    "app.js",
+    "/favicon.ico"
+];
+const CACHE_KEYS = [
+    CACHE_NAME
+  ];
+
+self.addEventListener('install', event => {
+    console.log(`install`);
+    event.waitUntil(
+        caches.open(CACHE_NAME) // 上記で指定しているキャッシュ名
+          .then(
+          function(cache){
+              return cache.addAll(urlsToCache); // 指定したリソースをキャッシュへ追加
+              // 1つでも失敗したらService Workerのインストールはスキップされる
+          })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(keys => {
+        return Promise.all(
+          keys.filter(key => {
+            return !CACHE_KEYS.includes(key);
+          }).map(key => {
+            // 不要なキャッシュを削除
+            return caches.delete(key);
+          })
+        );
+      })
+    );
+  });
+
+self.addEventListener('fetch', event => {
+    console.log(`fetch`);
+    var online = navigator.onLine;
+
+    //回線が使えるときの処理
+    if(online){
+        console.log(`online`);
+        event.respondWith(
+        caches.match(event.request)
+            .then(
+            function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request)
+                .then(function(response){
+                    cloneResponse = response.clone();
+                    if(response){
+                        if(response || response.status == 200){
+                            //キャッシュに追加
+                            caches.open(CACHE_NAME)
+                                .then(function(cache)
+                                {
+                                cache.put(event.request, cloneResponse)
+                                    .then(function(){
+                                        //正常にキャッシュ追加できたときの処理(必要であれば)
+                                        console.log(`cache added`);
+                                    });
+                                });
+                        }else{
+                            //正常に取得できなかったときにハンドリングしてもよい
+                            console.log(`cache failed`);
+                            return response;
+                        }
+                        return response;
+                    }
+                }).catch(function(error) {
+                    //デバッグ用
+                    return console.log(error);
+                });
+            })
+        );
+    }else{
+        //オフラインのときの制御
+        console.log(`offline`);
+        event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+            // キャッシュがあったのでそのレスポンスを返す
+            if (response) {
+                return response;
+            }
+            //オフラインでキャッシュもなかったパターン
+            return caches.match("index.html")
+                .then(function(responseNodata)
+                {
+                    return responseNodata;
+                });
+            }
+        )
+        );
+    }
+});
