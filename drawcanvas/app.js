7f5268c929728625d1871aefa242ccccead64324
mrakahel <k.yama118@gmail.com>
2021-12-04 01:42:34 +0900

    fix

--- drawcanvas/app.js
+++ drawcanvas/app.js
@@ -1,11 +1,69 @@
 //
 // JavaScriptのグローバル変数群
 //
+  
+let bluetoothDevice;
+let characteristic;
+let vercharacteristic;
+
+let TEXT_SERVICE_UUID = 'b07ff626-4b78-0001-89e5-fae40ab7e07f';
+let TEXT_CHARACTERISTIC_UUID = 'b07ff626-4b78-0004-89e5-fae40ab7e07f';
+let VERSION_CHARACTERISTIC_UUID = 'b07ff626-4b78-0002-89e5-fae40ab7e07f';
+
+let status;
+
 let CANVAS_SIZE;
 let undoDataStack = [];
 let redoDataStack = [];
 let mouseDown = false;
 let drawCount = 0;
+
+//ボタンイベントリスナー
+d3.select("#connect").on("click", connect);
+d3.select("#disconnect").on("click", disconnect);
+
+
+//デバイスに接続する
+async function connect() {
+    let options = {};
+
+    options.filters = [
+        {services: [TEXT_SERVICE_UUID]}
+    ];
+
+    updateStatus('Connecting...');
+
+    let server;
+    let service;
+    let chara;
+    try{
+        bluetoothDevice = await navigator.bluetooth.requestDevice(options)
+        console.log("device", bluetoothDevice);
+        updateDevicename(bluetoothDevice.name);
+        bluetoothDevice.ongattserverdisconnected = onGattServerDisconnected;
+        server = bluetoothDevice.gatt.connect();
+    
+        if(bluetoothDevice.gatt.connected){
+            server = bluetoothDevice.gatt;
+        }else{
+            server = await bluetoothDevice.gatt.connect();
+        }
+        console.log("server", server);
+        service = await server.getPrimaryService(TEXT_SERVICE_UUID);
+        console.log("service", service);
+        chara = await service.getCharacteristic(VERSION_CHARACTERISTIC_UUID)
+        vercharacteristic = chara;
+        console.log("characteristic", chara);    
+        chara = await service.getCharacteristic(TEXT_CHARACTERISTIC_UUID)
+        characteristic = chara;
+        console.log("characteristic", chara);
+        //alert("BLE接続が完了しました。");
+        updateStatus('Connected');
+    }catch(error){
+        console.log(error);
+        updateStatus(error);
+    }
+}
 
 $(function() {
     //
@@ -136,7 +194,23 @@ function drawing(event){
         y = event.clientY - wbound.top;
         draw(x, y);
     
+        let buf;
+        if(true){
+            buf = new ArrayBuffer(7);
+            buf[0] = 0x00;
+            arr_n = new Uint16Array(1);
+            arr_n = num;
+            arr_x = new Uint16Array(1);
+            arr_x = Math.floor(x);
+            arr_y = new Uint16Array(1);
+            arr_y = Math.floor(y);
+            buf.set(arr_n, 1);
+            buf.set(arr_x, 3);
+            buf.set(arr_y, 5);
+        }
+
         // Send 
+        sendData(buf);
     }
 }
 
@@ -185,3 +259,114 @@ function update(x, y){
     x_span.textContent = x;
     y_span.textContent = y;
 }
+
+
+//BLE切断処理
+function disconnect() {
+    if (!bluetoothDevice || !bluetoothDevice.gatt.connected){
+        console.log("device", bluetoothDevice);
+        return;
+    } 
+    bluetoothDevice.onGattServerDisconnected = "";
+    bluetoothDevice.gatt.disconnect();
+    bluetoothDevice = undefined;
+    updateDevicename("");
+    updateStatus("Disconnected");
+    //alert("BLE接続を切断しました。");
+}
+
+async function reconnect() {
+    if (!bluetoothDevice) {
+        console.log("device is undefined");
+        return;
+    }
+    console.log("device", bluetoothDevice);
+    let server;
+    let service;
+    let chara;
+    try{
+        if(bluetoothDevice.gatt.connected){
+            server = bluetoothDevice.gatt;
+        }else{
+            server = await bluetoothDevice.gatt.connect();
+        }
+        console.log("server", server);
+        service = await server.getPrimaryService(TEXT_SERVICE_UUID);
+        console.log("service", service);
+        chara = await service.getCharacteristic(TEXT_CHARACTERISTIC_UUID)
+        console.log("characteristic", chara);
+        updateStatus('Connected');
+        characteristic = chara; 
+        return true;   
+    }catch(error){
+        console.log(error);
+        updateStatus(error);
+    }
+    return false;
+}
+
+async function sendData(buf) {
+    let senddata;
+
+    arr = new Uint8Array(buf.byteLength)
+    arr.set(new Int8Array(buf), 0);
+    senddata = arr;
+    await characteristic.writeValueWithResponse(senddata);
+
+    return true;
+}
+
+
+function updateStatus(state) {
+    let elm = document.getElementById('status');
+    elm.textContent = state;
+}
+
+function updateDevicename(name) {
+    let elm = document.getElementById('devicename');
+    elm.textContent = name;
+}
+
+function clearText() {
+    document.querySelector("#message").value = "";
+}
+
+async function onAvailabilityChanged() {
+    let availability = await navigator.bluetooth.getAvailability();
+    if(!availability) {
+        alert("Bluetooth not available");
+        updateStatus("Disconnected");
+    }
+}
+
+async function onGattServerDisconnected() {
+    const maxretry = 3;
+    if(bluetoothDevice) {
+        updateStatus("Reconnecting...")
+        for(let step = 0; step < maxretry; step++){
+            if(await reconnect()) return;
+        }
+    }
+    updateStatus("Disconnected");
+}
+
+String.prototype.bytes = function () {
+    return(encodeURIComponent(this).replace(/%../g,"x").length);
+}
+
+window.addEventListener('load', async e => {
+    if("serviceWorker" in navigator){
+        try{
+            navigator.serviceWorker.register('sw.js')
+            .then(registratioin => {
+                registratioin.onupdatefound = function() {
+                    registratioin.update();
+                }
+            })
+            console.log(`SW registered`);
+        }catch(error){
+            console.log(`SW not registered`);
+        }
+    }
+    navigator.bluetooth.onavailabilitychanged = onAvailabilityChanged;
+});
